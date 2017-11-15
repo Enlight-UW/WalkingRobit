4d4a9e19a4b3203ce527e1576d71c70cbf0a5575
mwestphall <w.matthew.he@gmail.com>
2017-11-14 18:50:41 -0600

    changed motor movey scheme

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -4,13 +4,16 @@
 #define Centered 90 //Hip is centered
 #define minROM 10 //Minimum Range of Motion
 #define maxROM 170 //Maximum Range of Motion
-#define nMoves 12
+#define END -1
 int pinArray[numberOfServos] = {2,3, 4,5, 6,7, 8,9}; //Add more pins as needed
 Servo servoArray[numberOfServos];//Init all servo motors
 int Positions[] = {10, 50, 90, 130, 170};
 
-int moveInstructions[] = {0,0, 1,0, 0,4, 6,0, 7,0, 6,4};
-
+int moveInstructions[] = {0,0, 1,0, 0,4, //knee1 up, leg1 forward, knee1 down 
+                          6,0, 7,0, 6,4, //knee3 up, leg3 forward, knee3 down
+                          0,0, 1,4, 0,4, //knee1 up, leg1 back, knee1 down
+                          6,0, 7,4, 6,4, //knee3 up, leg3 back, knee3 down
+                          END,END};
 int i = 0;
 
 void moveMotor(int motoNumber, int pos){//pos is in degrees and will move TO the value given
@@ -38,5 +41,7 @@ void setup() {
 
 void loop() {  
   moveMotor(moveInstructions[i],moveInstructions[i+1]);
-  i = (i+2)%nMoves; 
+  i += 2;
+  if(moveInstructions[i]==END)
+    i=0; 
 }
