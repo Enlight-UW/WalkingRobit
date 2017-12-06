03c79170f21ebd4b84c3ef4af5a47b6a64836aca
Important Yogurt <mrska552@gmail.com>
2017-12-05 19:06:03 -0600

    god left me unfinished
    dicks

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -6,11 +6,15 @@
 #define maxROM 170 //Maximum Range of Motion
 #define END -1
 #define nPositions 4
-int pinArray[numberOfServos] = {2 ,3, 4,5, 6,7, 8, 9}; //Add more pins as needed
-Servo servoArray[numberOfServos];//all servo motors, initialized in setup
+int pinArray[numberOfServos] = {2 ,3, 4, 5, 6, 7, 8, 9}; //GPIO Pins used
+Servo servoArray[numberOfServos];//all servo motors in one array, initialized in setup
 
-int legPositions[] = {25, 170, 170, 25}; //Degrees of motion that legs/knees can mvoe to
-int kneePositions[] = {90, 10, 90, 90};
+//int legPositions[] = {25, 170, 170, 25}; //Degrees of motion that legs/knees can mvoe to
+int legPositions[][4] = {{25, 170, 170, 25}, {25, 170, 170, 25}, {170, 25, 25, 170}, {170, 25, 25, 170}};
+                                         //Make this a 2D array, 2 and maybe 3 are reverse of 0 and 1
+//int kneePositions[] = {90, 10, 90, 90};  //There are 3 90's, do not question or you will be sent to gulag
+int kneePositions[][4] = {{90, 10, 90, 90}, {90, 10, 90, 90}, {180, 0, 180, 180}, {180, 0, 180, 180}};
+//int kneePositions[] = {180, 270, 180, 180};
 
 int motorInstructions[] = {1, 1, END}; //Which motor to move
 int moveInstructions[]  = {0, 4, END}; //Where to move it
@@ -21,11 +25,11 @@ void moveMotor(int motoNumber, int pos){//pos is in degrees and will move TO the
 }
 
 void moveKnee(int knee, int posIdx){
-   moveMotor(knee*2,kneePositions[posIdx]); 
+   moveMotor(knee*2,kneePositions[knee][posIdx]); 
 }
 
 void moveLeg(int leg, int posIdx){
-   moveMotor(leg*2+1,legPositions[posIdx]); 
+   moveMotor(leg*2+1,legPositions[leg][posIdx]); 
 }
 
 void step(int legNumber){
@@ -38,12 +42,6 @@ void step(int legNumber){
     } 
     moveLeg(legNumber,0);
     moveKnee(legNumber,0);
-}
-
-//Step function to be called by walk function
-void walkStep(int legNumber, int position) {
-  moveLeg(legNumber, position);
-  moveKnee(legNumber, position);
 }
 
 //Takes a full step with all four legs
@@ -59,12 +57,22 @@ void setup() {
   for(int x = 0; x < numberOfServos; x++){//Assign each servo to a pin, array[0] = pinarray[0]
     servoArray[x].attach(pinArray[x]);
   }
- 
+
+  getShitSetUp();
+  delay(3000);
 }
 
+void getShitSetUp(){ //comments are for communists
+
+  for(int i=0; i<=3; i++){
+    moveLeg(i, 0);
+    moveKnee(i, 0);
+    delay(100);
+  }
+}
 
 void loop() {  
   //Test: Loop through all the positions on a specific motor
-  //step(0);
-  walk();
+  step(0);  //1 is broked
+  //walk();
 }
