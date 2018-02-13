0f2607fe7bd53e527fba0403e382445413d18985
michaelciancimino <michaelciancimino@gmail.com>
2018-02-13 13:51:48 -0600

    Keyboard listener added. Robot should move incrementally

--- ServoTest/ServoTest.ino
+++ ServoTest/ServoTest.ino
@@ -6,19 +6,61 @@
 #define maxROM 170 //Maximum Range of Motion
 #define END -1
 #define nPositions 4
+#define DOWN 90
+#define RDOWN 180
+#define UP 10
+#define RUP 0
+#define BACK 25
+#define RBACK 170
+#define FORWARD 170
+#define RFORWARD 25
+
+//byte typed by keyboard
+byte byteRead;
+char keyRead;
+
 int pinArray[numberOfServos] = {2 ,3, 4, 5, 6, 7, 8, 9}; //GPIO Pins used
 Servo servoArray[numberOfServos];//all servo motors in one array, initialized in setup
 
 //int legPositions[] = {25, 170, 170, 25}; //Degrees of motion that legs/knees can mvoe to
-int legPositions[][4] = {{25, 170, 170, 25}, {25, 170, 170, 25}, {170, 25, 25, 170}, {170, 25, 25, 170}};
+int legPositions[][4] = {{FORWARD, 120, 80, BACK}, {120, RBACK, RFORWARD, 80}, {BACK, FORWARD, 120, 80}, {80, 120, RBACK, RFORWARD}};
                                          //Make this a 2D array, 2 and maybe 3 are reverse of 0 and 1
-//int kneePositions[] = {90, 10, 90, 90};  //There are 3 90's, do not question or you will be sent to gulag
-int kneePositions[][4] = {{90, 10, 90, 90}, {90, 10, 90, 90}, {180, 0, 180, 180}, {180, 0, 180, 180}};
-//int kneePositions[] = {180, 270, 180, 180};
+//int kneePositions[][4] = {{UP, DOWN, UP, UP}, {180, 0, 180, 180}, {UP, DOWN, UP, UP}, {180, 0, 180, 180}};
+int kneePositions[][4] = {{UP, DOWN, DOWN, DOWN}, {RDOWN, RDOWN, RUP, RDOWN}, {DOWN, UP, DOWN, DOWN}, {RDOWN, RDOWN, RDOWN, RUP}};
 
 int motorInstructions[] = {1, 1, END}; //Which motor to move
 int moveInstructions[]  = {0, 4, END}; //Where to move it
 int i = 0;
+
+//current position within sequence
+int currKneePos[4] = {0,0,0,0};
+int currLegPos[4] = {0,0,0,0};
+
+//pos - position of leg or knee
+//isLeg - 0 if leg is being moved, else 1
+//Changes position of chosen leg or knee then moves the appropriate part
+void moveRobot(int pos, int isLeg) {
+  if(pos < 0 || pos > 3) {
+    Serial.println("Error: moveRobot incorrect usage of pos");
+  }
+  if(isLeg == 0) {
+     currLegPos[pos]++;
+     if(currLegPos[pos] >= sizeof(legPositions[pos])) {
+      currLegPos[pos] = 0;
+     }
+     moveLeg(pos, currLegPos[pos]);
+  }
+  else if(isLeg == 1) {
+    currKneePos[pos]++;
+     if(currKneePos[pos] >= sizeof(kneePositions[pos])) {
+      currKneePos[pos] = 0;
+     }
+     moveKnee(pos, currKneePos[pos]);
+  }
+  else {
+    Serial.println("Error: moveRobot incorrect usage of isLeg");
+  }
+}
 
 void moveMotor(int motoNumber, int pos){//pos is in degrees and will move TO the value given
   servoArray[motoNumber].write(pos); 
@@ -38,7 +80,7 @@ void step(int legNumber){
     for(j=0;j<nPositions;j++){
         moveLeg(legNumber,j);
         moveKnee(legNumber,j); 
-        delay(200);
+        delay(1000);
     } 
     moveLeg(legNumber,0);
     moveKnee(legNumber,0);
@@ -46,10 +88,18 @@ void step(int legNumber){
 
 //Takes a full step with all four legs
 void walk() {
-  step(0);
-  step(2);
+  /*step(0);
   step(1);
-  step(3);
+  step(2);
+  step(3);*/
+  for(int pos=0;pos<nPositions;pos++){
+    for(int legNumber = 0; legNumber < 4; legNumber++) {
+      moveLeg(legNumber,pos);
+      moveKnee(legNumber,pos);
+    }   
+      delay(100);
+  }
+  
 }
 
 void setup() {
@@ -59,6 +109,10 @@ void setup() {
   }
 
   getShitSetUp();
+
+  // turn on serial protocol
+  Serial.begin(9600);
+
   delay(3000);
 }
 
@@ -71,8 +125,44 @@ void getShitSetUp(){ //comments are for communists
   }
 }
 
+//wait for keyboard input and respond
 void loop() {  
-  //Test: Loop through all the positions on a specific motor
-  step(0);  //1 is broked
-  //walk();
+
+  //check for key input
+  if (Serial.available()) {
+    
+    //read most recent byte
+    byteRead = Serial.read();
+    keyRead = byteRead;
+    
+    if(keyRead == 'A'){
+      moveRobot(0, 1);
+    }
+    else if(keyRead == 'S') {
+      moveRobot(1, 1);
+    }
+    else if(keyRead == 'D') {
+      moveRobot(2, 1);
+    }
+    else if(keyRead == 'F') {
+      moveRobot(3, 1);
+    }
+    else if(keyRead == 'H') {
+      moveRobot(0, 0);
+    }
+    else if(keyRead == 'J') {
+      moveRobot(1, 0);
+    }
+    else if(keyRead == 'K') {
+      moveRobot(2, 0);
+    }
+    else if(keyRead == 'L') {
+      moveRobot(3, 0);
+    }
+    else{
+      //Unsupported key
+      Serial.println("Bad key press: " + keyRead);
+    }
+  }
 }
+
