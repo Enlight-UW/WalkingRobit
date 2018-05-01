17b7fc3322cb6137b5a563be284111eed536ac65
Vinay Janardhanam <vjanard@gmail.com>
2018-05-01 18:13:21 -0500

    Walking robot controller code
    Only sends out a value upon an update

---
+++ walker_controller.ino
@@ -0,0 +1,42 @@
+int button[] = {3,5,7,9,2,4,6,8};
+byte output = 0;
+int buttonState[] = {0,0,0,0,0,0,0,0};
+int val = 0;
+byte oldOutput = 420;
+
+
+void setup() {
+  for(int i=0;i<8;i++){
+    pinMode(button[i],INPUT_PULLUP);
+  }
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  for(int i=0;i<8;i++){
+    val = digitalRead(button[i]);
+    if(val == LOW){
+      buttonState[i] = 1;
+    }else{
+      buttonState[i] = 0;
+    }
+  }
+
+  for(int i=0;i<8;i++){
+    if(buttonState[i] == 1){
+      bitSet(output,i);
+    }else{
+      bitClear(output,i);
+    }
+  }
+
+  //if(output != 0){
+    if (output != oldOutput){
+      Serial.println(output);
+      oldOutput = output;
+    }
+    //delay(100);
+  //}
+
+}
